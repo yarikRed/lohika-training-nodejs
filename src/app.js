e2b50040e97aaf9420fccfc75ffca9d7fd409408
Yaroslav Shkuratenko <yshkuratenko@lohika.com>
2021-10-26 18:03:30 +0300

    config object imported to app.js from config.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const config = require('./config');
 
+const PORT = config.APP_PORT || 3005;
 http.createServer((req, res) => {
     console.log('New incoming request');
     res.writeHeader(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: 'Hello world!' }));
-}).listen(3005, () => console.log('Listening on port 3005...'));
+}).listen(PORT, () => console.log(`Listening on port ${PORT}...`));
---
+++ src/config.js
@@ -0,0 +1,9 @@
+const ENV = process.argv;
+const APP_PORT = process.env.APP_PORT;
+
+const config = {
+    ENV,
+    APP_PORT,
+};
+
+module.exports = config;
