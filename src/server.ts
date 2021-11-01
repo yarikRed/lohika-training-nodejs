44611f601118b0dc55314e569ebec774b10d4430
Yaroslav Shkuratenko <yshkuratenko@lohika.com>
2021-11-01 16:04:29 +0200

    migrated to typescript

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-const http = require('http');
-const config = require('./config');
-const logger = require('./logger');
+import http from 'http';
+import { config } from './config';
+import { logger } from './logger';
 
 const {
     APP_PORT: PORT,
     ENV,
 } = config;
 
-http.createServer((req, res) => {
+export const server = http.createServer((req, res) => {
     console.log('New incoming request');
-    res.writeHeader(200, { 'Content-Type': 'application/json' });
+    res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: 'Hello world!' }));
 }).listen(PORT, () => logger.log(`Listening on port ${PORT}... Env is ${ENV}`));
